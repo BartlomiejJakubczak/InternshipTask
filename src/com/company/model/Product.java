dae25dfbffb9bfa76444b123e48b68deb186455b
Bartłomiej Jakubczak <bjakubczak96@gmail.com>
2019-04-13 13:48:16 +0200

    Add Product model implementation

--- src/com/company/model/Product.java
+++ src/com/company/model/Product.java
@@ -2,4 +2,37 @@ package com.company.model;
 
 public class Product {
 
+    private String name;
+    private int price;
+    private double barcode;
+
+    public Product(String name, int price, double barcode) {
+        this.name = name;
+        this.price = price;
+        this.barcode = barcode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public double getBarcode() {
+        return barcode;
+    }
+
+    public void setBarcode(double barcode) {
+        this.barcode = barcode;
+    }
 }
