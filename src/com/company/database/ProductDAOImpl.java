ccdbf6879b135b36a4539b3b04d3d3642f355c1f
Bartłomiej Jakubczak <bjakubczak96@gmail.com>
2019-04-13 15:01:43 +0200

    Update ProductDAOImpl

--- src/com/company/database/ProductDAOImpl.java
+++ src/com/company/database/ProductDAOImpl.java
@@ -2,13 +2,24 @@ package com.company.database;
 
 import com.company.model.Product;
 
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProductDAOImpl implements ProductDAO {
 
     private static ProductDAOImpl instance;
+    private List<Product> products;
 
     private ProductDAOImpl() {
+        Product cheese = new Product(1, "Cheese", 4.49, 111111111111L);
+        Product ham = new Product(2, "Ham", 5.49, 222222222222L);
+        Product cannedPineapple = new Product(3, "Canned Pineapple", 3.60, 33333333333L);
+        Product potatoChips = new Product(4, "Potato Chips", 6.99, 444444444444L);
+        products = new ArrayList<>();
+        products.add(cheese);
+        products.add(ham);
+        products.add(cannedPineapple);
+        products.add(potatoChips);
     }
 
     public static ProductDAOImpl getInstance() {
@@ -21,12 +32,15 @@ public class ProductDAOImpl implements ProductDAO {
     }
 
     @Override
-    public Set<Product> getAllProducts() {
-        return null;
+    public List<Product> getAllProducts() {
+        return products;
     }
 
     @Override
-    public Product getProduct(double barcode) {
-        return null;
+    public Product findByBarcode(long barcode) {
+        return products.stream()
+                .filter(product -> barcode == product.getBarcode())
+                .findAny()
+                .orElse(null);
     }
 }
