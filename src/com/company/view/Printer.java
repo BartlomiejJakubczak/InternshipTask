c169998a5a260eb395d733b4baea980891dc1e1b
Bartłomiej Jakubczak <bjakubczak96@gmail.com>
2019-04-14 21:39:29 +0200

    Modify views

--- src/com/company/view/Printer.java
+++ src/com/company/view/Printer.java
@@ -7,9 +7,10 @@ import java.util.List;
 public class Printer {
     public void printShoppingList(List<Product> scannedProducts, double totalSum) {
         System.out.println("PRODUCTS LIST\n");
+        System.out.format("%16s%10s\n", "NAME OF PRODUCT", "PRICE");
         for(Product product: scannedProducts) {
             System.out.format("%16s%10s\n", product.getName(), String.format("%.2f", product.getPrice()));
         }
-        System.out.println("\nTotal sum: " + totalSum);
+        System.out.format("%16s%15s", "\nTotal sum: ", totalSum);
     }
 }
